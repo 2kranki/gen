c966656e3003c48d04a47cae0ee778b887835941
bob <kranki@me.com>
2019-07-23 10:16:38 -0400

    Set up test01 to use mssql, added docker pull to mssql's run.sh

--- src/models/sqlapp/dbs/mssql/run.sh
+++ src/models/sqlapp/dbs/mssql/run.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
+# add -xv above after bash to debug
 
 name="mssql1"
 user="SA"
 pw="Passw0rd!"
 server="localhost"
 port=1433
+dockerName="mcr.microsoft.com/mssql/server"
+dockerTag="2017-latest-ubuntu"
 
-echo "Ignore message: Error: No such container: mssql1"
-docker container rm -f ${name}
+imageName="${dockerName}"
+if [ -n "${dockerTag}" ]; then
+    imageName="${dockerName}:${dockerTag}"
+fi
+#echo "Image Name: ${imageName}"
 
-containerID=`docker container run --name ${name} -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=${pw}" -p ${port}:1433  -d mcr.microsoft.com/mssql/server:2017-latest`
+#echo "Ignore message: Error: No such container: mssql1"
+s=`docker container rm -f ${name} 2>1`
+
+if docker image ls ${imageName} | tail -n 1 | grep "${dockerName}"; then
+    :
+else
+    docker image pull "${imageName}"
+fi
+
+containerID=`docker container run --name ${name} -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=${pw}" -p ${port}:1433  -d "${imageName}"`
 #echo "Container ID: ${containerID: -10}"
 
 while ! `nc -z ${server} ${port}`; do sleep 3; done
